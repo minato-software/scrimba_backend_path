606c1a05c96c4b284f764e1caf82637b2dfe06c7
Zach Groves <fluffygorawr14@gmail.com>
2026-04-17 15:43:53 -0500

    'Node Content-Type'

--- s0qkhpdhiu/server.js
+++ s0qkhpdhiu/server.js
@@ -1,13 +1,25 @@
 import http from 'node:http'
+import {getDataFromDB} from "./database/db.js";
 
 const PORT = 4000
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
+
     if (req.url === '/api' && req.method === 'GET') {
-
-        res.end('This is from the server.')
-
+       try{
+           const dbData = await getDataFromDB();
+           const destinations = JSON.stringify(dbData);
+           res.writeHead(200, {'Content-Type': 'application/json'});
+           res.end(destinations);
+       } catch(err) {
+           console.error(err);
+           res.writeHead(500, { 'Content-Type': 'application/json' });
+           res.end(JSON.stringify({ error: 'Internal Server Error' }));
+       }
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end("NOT FOUND");
     }
-})
+});
 
 server.listen(PORT, () => console.log(`listening on port ${PORT}`))
