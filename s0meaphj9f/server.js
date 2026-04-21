2bb818266f2dc9aa98b6b49a86767ab6b5adf9c6
Zach Groves <fluffygorawr14@gmail.com>
2026-04-20 22:28:12 -0500

    'Setting up the project'

---
+++ s0meaphj9f/server.js
@@ -0,0 +1,11 @@
+import http from 'node:http'
+
+const PORT = 4000
+
+const server = http.createServer(async (req, res) => {
+    const serveString = '<html><h1>The server is working</h1></html>';
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(serveString);
+})
+
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`))
