3749d3ac74bba4532e91446ff6628483249a776a
Zach Groves <fluffygorawr14@gmail.com>
2026-04-20 21:00:54 -0500

    'Get the Query Parameters'

--- s0qkhpdhiu/server.js
+++ s0qkhpdhiu/server.js
@@ -1,23 +1,36 @@
 import http from 'node:http'
 import {getDataFromDB} from "./database/db.js";
 import { sendJSON } from "./utils/utils.js";
+import { destinationFilter } from "./utils/utils.js";
 
 const PORT = 4000
 
 const server = http.createServer(async (req, res) => {
     const destinations = await getDataFromDB();
 
-    if (req.url === '/api' && req.method === 'GET') {
+    const urlObj = new URL(req.url, `http://${req.headers.host}`);
+
+    const queryObj = Object.fromEntries(urlObj.searchParams);
+
+
+
+    if (urlObj.pathname === '/api' && req.method === 'GET') {
+        //let filteredDestinations = destinations;
+
+        console.log(queryObj);
+        // update filteredDestinations
+
         sendJSON(res, 200, destinations)
 
     } else if (req.url.startsWith('/api/continent/') && req.method === 'GET') {
-        const continentName = req.url.split('/').pop();
-        const filteredDestinations = destinations.filter((destination) => {
-            return destination.continent.toLowerCase() === continentName.toLowerCase();
-        })
+        const continent = req.url.split('/').pop();
+        const filteredDestinations = destinationFilter(destinations, 'continent', continent);
+        sendJSON(res, 200, filteredDestinations);
 
+    } else if (req.url.startsWith('/api/country/') && req.method === 'GET') {
+        const country = req.url.split('/').pop();
+        const filteredDestinations = destinationFilter(destinations, 'country', country);
         sendJSON(res, 200, filteredDestinations)
-
     } else {
 
         sendJSON(res, 404, {
