475dda9f1cb851fbe062e408d12028b9dbee78b2
Zach Groves <fluffygorawr14@gmail.com>
2026-04-27 23:23:05 -0500

    'parseJSONBody'

---
+++ s06d2mp5rb/handlers/routeHandlers.js
@@ -0,0 +1,21 @@
+import { getData } from '../utils/getData.js'
+import { sendResponse } from '../utils/sendResponse.js'
+import {parseJSONBody} from "../utils/parseJSONBody.js";
+
+
+export async function handleGet(res) {
+  const data = await getData()
+  const content = JSON.stringify(data)
+  sendResponse(res, 200, 'application/json', content)
+}  
+
+export async function handlePost(req) {
+  const rawBody = await parseJSONBody(req)
+  console.log(rawBody)
+} 
+
+// santizeData() 
+// addNewSighting() will do the donkey work of adding the data to our dataset
+// sendResponse()
+
+
